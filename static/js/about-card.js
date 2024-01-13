7c7dbad096f19c31c4ffc6d4e86a59f626a8c370
XuLiangQi <xuliang.qi@outlook.com>
2024-01-13 12:52:52 -0500

    update about-card js

--- static/js/about-card.js
+++ static/js/about-card.js
@@ -2,11 +2,10 @@ function createAboutCard(title, details) {
     var aboutCardContainer = document.getElementById("about-card-container");
     aboutCardContainer.className = "about-card-container";
 
-    // Create elements for the new card
     var aboutCard = document.createElement("div");
     aboutCard.className = "about-card";
 
-    var aboutTitle = document.createElement("div");
+    var aboutTitle = document.createElement("h1");
     aboutTitle.className = "about-card-title";
     aboutTitle.textContent = title;
 
@@ -14,18 +13,18 @@ function createAboutCard(title, details) {
 
     if (details) {
         details.forEach(detail => {
-            var subTitle = document.createElement("div");
+            var subTitle = document.createElement("h2");
             subTitle.className = "about-card-sub-title";
             subTitle.textContent = detail.subTitle;
 
             aboutCard.appendChild(subTitle);
 
             detail.contents.forEach(content => {
-                var contentDiv = document.createElement("div");
-                contentDiv.className = "about-card-content";
-                contentDiv.textContent = content;
+                var subContent = document.createElement("p");
+                subContent.className = "about-card-content";
+                subContent.textContent = content;
 
-                aboutCard.appendChild(contentDiv);
+                aboutCard.appendChild(subContent);
             });
         });
     }
