0ac5f029937e87185d535274fc67a859e846fe74
XuLiangQi <xuliang.qi@outlook.com>
2024-03-09 12:51:20 -0500

    formatting css, js, and html

--- static/js/about-card.js
+++ static/js/about-card.js
@@ -1,34 +1,34 @@
 function createAboutCard(title, details) {
-    var aboutCardContainer = document.getElementById("about-card-container");
-    aboutCardContainer.className = "about-card-container";
+  var aboutCardContainer = document.getElementById("about-card-container");
+  aboutCardContainer.className = "about-card-container";
 
-    var aboutCard = document.createElement("div");
-    aboutCard.className = "about-card";
+  var aboutCard = document.createElement("div");
+  aboutCard.className = "about-card";
 
-    var aboutTitle = document.createElement("h1");
-    aboutTitle.className = "about-card-title";
-    aboutTitle.textContent = title;
+  var aboutTitle = document.createElement("h1");
+  aboutTitle.className = "about-card-title";
+  aboutTitle.textContent = title;
 
-    aboutCard.appendChild(aboutTitle);
+  aboutCard.appendChild(aboutTitle);
 
-    if (details) {
-        details.forEach(detail => {
-            var subTitle = document.createElement("h2");
-            subTitle.className = "about-card-sub-title";
-            subTitle.textContent = detail.subTitle;
+  if (details) {
+    details.forEach((detail) => {
+      var subTitle = document.createElement("h2");
+      subTitle.className = "about-card-sub-title";
+      subTitle.textContent = detail.subTitle;
 
-            aboutCard.appendChild(subTitle);
+      aboutCard.appendChild(subTitle);
 
-            detail.contents.forEach(content => {
-                var subContent = document.createElement("p");
-                subContent.className = "about-card-content";
-                subContent.textContent = content;
+      detail.contents.forEach((content) => {
+        var subContent = document.createElement("p");
+        subContent.className = "about-card-content";
+        subContent.textContent = content;
 
-                aboutCard.appendChild(subContent);
-            });
-        });
-    }
+        aboutCard.appendChild(subContent);
+      });
+    });
+  }
 
-    // Append the about card to the about card container
-    aboutCardContainer.appendChild(aboutCard);
+  // Append the about card to the about card container
+  aboutCardContainer.appendChild(aboutCard);
 }
